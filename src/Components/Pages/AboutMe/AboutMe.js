d1fe3700d58d4cd0fdaaf26db39b5d3f413f0f3d
Ornella <ornella.sciarrillo@telefonica.com>
2021-02-22 19:39:56 -0300

    arreglos deploy

--- src/Components/Pages/AboutMe/AboutMe.js
+++ src/Components/Pages/AboutMe/AboutMe.js
@@ -4,16 +4,18 @@ import emoji1 from '../../Assets/emoji1.png';
 
 const AboutMe = () => {
 
-    let text='Hola! Que gusto tenerte navegando por mi portfolio web. Aquí va un resumen sobre mí:'
-    let text2='Me egresé en noviembre del 2020 como Desarrolladora Frontend Jr. en ADA ITW'
-    let text3='Desde ese momento mi objetivo siempre fue seguir capacitándome día a día para crecer de manera personal y profesional en este nuevo mundo IT para mi'
-    let text4='Actualmente estoy trabajando en una prestigiosa empresa multinacional llamada Teléfonica. Soy parte del equipo de desarrollo de un nuevo proyecto que tiene la compañia'
-    let text5='Estoy súper feliz y agradecida. Espero que te sientas cómodo/a navegando por mi web y no dudes en contactarme, estoy dispuesta a aceptar siempre nuevos desafíos'
+    let text='Hola, cómo estás? Un gusto tenerte navegando por mi portfolio web!'
+    let text1=' Me dicen Orne, tengo 26 años y soy de C.A.B.A., Argentina.'
+    let text2='Me egresé en noviembre del 2020 como Desarrolladora Frontend Jr. en ADA ITW.'
+    let text3='Desde ese momento mi objetivo siempre fue seguir capacitándome día a día para crecer de manera personal y profesional en el mundo IT.'
+    let text4='Actualmente estoy trabajando en una prestigiosa empresa multinacional llamada Teléfonica. Soy parte del equipo de desarrollo de un nuevo proyecto que tiene la compañia y estoy orgullosa de ser parte.'
+    let text5='Estoy súper feliz y agradecida. Espero que te sientas cómodo/a navegando por mi web y no dudes en contactarme, estoy dispuesta a aceptar opiniones para mejorar mi código y también por qué no, nuevos desafíos...'
     return (
         <div className='aboutMe'>
             <div className='aboutMe-content'>
                 {/* <img src={emoji1} alt='Perfil' className='index-logo'></img> */}
                 <p className='aboutMe-p'>{text}</p>
+                <p className='aboutMe-p'>{text1}</p>
                 <p className='aboutMe-p'>{text2}</p>
                 <p className='aboutMe-p'>{text3}</p>
                 <p className='aboutMe-p'>{text4}</p>
--- src/Components/Pages/ContactMe/ContactMe.js
+++ src/Components/Pages/ContactMe/ContactMe.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './ContactMe.scss';
+import emoji3 from '../../Assets/emoji3.png';
 
 const ContactMe = () => {
     return (
-        <h1>Contact Me</h1>
+        <div className='contactMe'>
+                <img src={emoji3} alt='Perfil' className='contactMe-logo'></img>
+                <a href='http://www.gmail.com'>ornee.sci@gmail.com</a>
+                
+        </div>
     )
 }
 
